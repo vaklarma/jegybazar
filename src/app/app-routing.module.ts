33af26ef54047f3e08641ab046d07f60724d0f86
vaklarma <biro.andrasgabor.regist@gmail.com>
2018-06-06 00:55:34 +0200

    * esemény lista és egyebek * routing children

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,30 +8,42 @@ import {createRouterState} from '@angular/router/src/create_router_state';
 import {AboutComponent} from './about/about.component';
 import {LoginComponent} from './login/login.component';
 import {RegistrationComponent} from './registration/registration.component';
+import {EventListComponent} from './event-list/event-list.component';
+import {EventDetailComponent} from './event-detail/event-detail.component';
 
 const routes: Routes = [
-  { path : 'home', component: HomeComponent},
-  { path : 'event', component: EventComponent},
-  { path: 'ticket', component: TicketComponent},
-  { path: 'about', component: AboutComponent},
-  { path: 'login', component: LoginComponent},
-  { path: 'registration', component: RegistrationComponent},
-  { path : '', redirectTo: '/home', pathMatch: 'full'},
-  { path : '**', component: PageNotFoundComponent},
+  {path: 'home', component: HomeComponent},
+  {
+    path: 'event',
+    component: EventComponent,
+    children: [
+      {path: 'list', component: EventListComponent},
+      {path: ':id/edit', component: EventDetailComponent},
+    ],
+  },
+  {path: 'ticket', component: TicketComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'registration', component: RegistrationComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent},
 ];
-@NgModule ({
+
+@NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 
 export class AppRoutingModule {
-static routableComponents = [
-  HomeComponent,
-  EventComponent,
-  TicketComponent,
-  AboutComponent,
-  LoginComponent,
-  RegistrationComponent,
-  PageNotFoundComponent,
-];
+  static routableComponents = [
+    HomeComponent,
+    EventComponent,
+    EventListComponent,
+    EventDetailComponent,
+    TicketComponent,
+    AboutComponent,
+    LoginComponent,
+    RegistrationComponent,
+    PageNotFoundComponent,
+  ];
 }
