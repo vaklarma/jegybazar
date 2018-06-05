5e06b66a5ff65eaaba31f27e9b1a4a2ed1a7895f
vaklarma <biro.andrasgabor.regist@gmail.com>
2018-06-05 21:50:02 +0200

    * teszt app comp.css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {CollapseModule} from 'ngx-bootstrap/collapse';
-import { NavbarComponent } from './navbar/navbar.component';
-import { JumbotronComponent } from './jumbotron/jumbotron.component';
-import { EventcardComponent } from './eventcard/eventcard.component';
-import { FooterComponent } from './footer/footer.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {JumbotronComponent} from './jumbotron/jumbotron.component';
+import {EventcardComponent} from './eventcard/eventcard.component';
+import {FooterComponent} from './footer/footer.component';
+import {LoginComponent} from './login/login.component';
+import {RegistrationComponent} from './registration/registration.component';
+import {ProfileComponent} from './profile/profile.component';
+import {ProfileEditComponent} from './profile-edit/profile-edit.component';
+import {EventComponent} from './event/event.component';
+import {EventDetailComponent} from './event-detail/event-detail.component';
+import {EventListComponent} from './event-list/event-list.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {AboutComponent} from './about/about.component';
+import {BidComponent} from './bid/bid.component';
+import {TicketDetailComponent} from './ticket-detail/ticket-detail.component';
+import {TicketListComponent} from './ticket-list/ticket-list.component';
+import {TicketComponent} from './ticket/ticket.component';
+
+import {AppRoutingModule} from './app-routing.module';
 
 
 @NgModule({
@@ -15,11 +30,16 @@ import { FooterComponent } from './footer/footer.component';
     NavbarComponent,
     JumbotronComponent,
     EventcardComponent,
-    FooterComponent
+    EventComponent,
+    FooterComponent,
+    ...AppRoutingModule.routableComponents
+
+
   ],
   imports: [
     BrowserModule,
-    CollapseModule.forRoot()
+    CollapseModule.forRoot(),
+    AppRoutingModule
 
   ],
   providers: [],
