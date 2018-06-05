19e21159cab4f0720eeacf3a3d761901604d9a9e
vaklarma <biro.andrasgabor.regist@gmail.com>
2018-06-05 22:54:15 +0200

    * routerlinkek beírása * navbar működik

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,23 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {EventComponent} from './event/event.component';
+import {TicketComponent} from './ticket/ticket.component';
+import {createRouterState} from '@angular/router/src/create_router_state';
+import {AboutComponent} from './about/about.component';
+import {LoginComponent} from './login/login.component';
+import {RegistrationComponent} from './registration/registration.component';
 
 const routes: Routes = [
   { path : 'home', component: HomeComponent},
+  { path : 'event', component: EventComponent},
+  { path: 'ticket', component: TicketComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'registration', component: RegistrationComponent},
+  { path : '', redirectTo: '/home', pathMatch: 'full'},
+  { path : '**', component: PageNotFoundComponent},
 ];
 @NgModule ({
   imports: [RouterModule.forRoot(routes)],
@@ -12,6 +26,12 @@ const routes: Routes = [
 
 export class AppRoutingModule {
 static routableComponents = [
-  HomeComponent
+  HomeComponent,
+  EventComponent,
+  TicketComponent,
+  AboutComponent,
+  LoginComponent,
+  RegistrationComponent,
+  PageNotFoundComponent,
 ];
 }
